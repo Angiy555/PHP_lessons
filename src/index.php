66139af4c3655e5c7898cd57792bef544dc16c1d
HOME\Дом <angiy.kaa555@yandex.ru>
2024-08-19 09:16:00 +0300

    Lesson_1

--- src/index.php
+++ src/index.php
@@ -6,3 +6,13 @@
   var_dump((int)'012345');
   var_dump((float)123.0 === (int)123.0);
   var_dump(0 == 'hello, world');
+
+  $x = 1;
+  $y = 2;
+
+  $x = $x + $y;
+  var_dump($y = $x - $y);
+  var_dump($x = $x - $y);
+
+  echo "X = ". $x;
+  echo " Y = ". $y;
